8c5c786437250695beb95c7a38d2d0e30ad3323b
shazidhasanriam <shazid.riam@northsouth.edu>
2021-11-17 19:58:09 +0600

    Initial Final Update

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 let apiQuotes = [];
 
+//Showing Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+//Hide Loader
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 //Showing new quote
 function newQuote() {
+  // loader
+  loading();
   //Picking a random quote from the API Array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  //check if there is no author
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //Checking the  quote length to determine styling
+  if (quote.text.lenght > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  //Set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
 }
-
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -16,9 +49,16 @@ async function getQuotes() {
     // console.log(apiQuotes);
     newQuote();
   } catch (error) {
-    // Catch errors here
+    // Catch the errors here
   }
 }
-
+//Tweet Quote Button
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+// Event Listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 //On Loading
 getQuotes();
